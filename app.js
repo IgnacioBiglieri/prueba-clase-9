91d868f65f3048c72f32944de969816be1cfc99d
Ignacio Biglieri <nacho.developerweb@gmail.com>
2025-06-06 17:55:46 -0300

    mensaje

--- app.js
+++ app.js
@@ -20,5 +20,19 @@ const numerosMultiplicados = numeros.map((num) => {
 console.log("el array modificado es " + numerosMultiplicados);
 
 
-//EJERCICIO 3
-// crear un array de objetos y filtrar los que tengan un precio mayor a 10
+//EJERCICIO 3 (prueba)
+
+
+//para crear el repo
+//1ro git init
+//2do git add .
+//3ro git commit -m "mensaje de commit"
+//4to git remote add origin <url del repo>
+//5to git branch -M main (lo saltean si ya tienen un main)
+//6to git push -u origin main
+
+// una vez creado el repo
+// 1ro git add .
+// 2do git commit -m "mensaje de commit"
+// 3ro git push
+
